734377ebd1f2493220cd070a63ff95aa5c7eddb1
admin <farhatd680@gmail.com>
2024-01-17 13:39:09 +0200

    Edit on boarding

--- lib/features/onboarding/presentation/wedgits/indicator.dart
+++ lib/features/onboarding/presentation/wedgits/indicator.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-class OnBoardingScreen extends StatelessWidget {
-  const OnBoardingScreen({super.key});
+class Indicator extends StatelessWidget {
+  const Indicator({super.key});
 
   @override
   Widget build(BuildContext context) {
